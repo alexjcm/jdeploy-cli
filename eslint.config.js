c2e923e8b6dc8d22c1175c560b1de333e966f17d
alexjcm <alexjhon@live.com>
2026-03-20 07:21:26 -0500

    feat: Add eslint config

---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    ignores: [
+      "node_modules/**",
+      "dist/**"
+    ],
+  },
+  {
+    files: ["src/**/*.ts"],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+    ],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        Bun: "readonly"
+      },
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      // "no-constant-condition" is disabled to allow the main CLI interactive while(true) loop.
+      "no-constant-condition": "off",
+      "@typescript-eslint/no-explicit-any": "warn"
+    }
+  }
+]);
